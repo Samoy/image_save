8b2edefa99fd46477ba5c4515819c18dc950505e
Samoy <samoy_young@163.com>
2020-03-23 18:13:50 +0800

    Format code.

--- lib/image_save.dart
+++ lib/image_save.dart
@@ -9,13 +9,13 @@ import 'package:flutter/services.dart';
 class ImageSave {
   static const MethodChannel _channel = const MethodChannel('image_save');
 
-	/// Save Image to album.
-	/// [imageType]: image type,such as jpg,gif and so on.
-	/// [imageData]:Image Data.
-	/// returns the path of image if image saved success.
-	/// ```dart
-	/// String path = await ImageSave.saveImage("gif", Uint8List.fromList(res.data));
-	/// ```
+  /// Save Image to album.
+  /// [imageType]: image type,such as jpg,gif and so on.
+  /// [imageData]:Image Data.
+  /// returns the path of image if image saved success.
+  /// ```dart
+  /// String path = await ImageSave.saveImage("gif", Uint8List.fromList(res.data));
+  /// ```
   static Future<String> saveImage(String imageType, Uint8List imageData) async {
     final String path = await _channel.invokeMethod(
         'saveImage', {"imageType": imageType, "imageData": imageData});
